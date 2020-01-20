e6c0ad526e39a3e59328b625cfba896413c5e0e2
Emily <bruner.emilym@gmail.com>
2020-01-20 15:01:28 -0500

    finished guided project

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,24 +5,61 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res, next) => {
+try {
+    res.json(await db.select("*").from("posts"))
+} catch (err) {
+    next(err)
+}
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res, next) => {
+    try {
+       const post = await db("posts").where("id", req.params.id).select()
+       res.json(post)
+    } catch (err) {
+        next(err)
+    }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
 
+        const [id] = await db("posts").insert(payload)
+        res.json(await db("posts").where("id", id).first())
+    } catch (err) {
+        next(err)
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
 
+        //update posts where title - ? and contents = ? where id = ?
+
+        await db("posts").where("id", req.params.id).update(payload)
+        res.json(await db("posts").where("id", req,params.id).first())
+
+    } catch (err) {
+        next(err)
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async(req, res, next) => {
+    try {
+        await db("posts").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,4 +12,9 @@ server.get('/', (req, res) => {
   res.send('<h3>DB Helpers with knex</h3>');
 });
 
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({message: "Something went wrong"})
+})
+
 module.exports = server;
